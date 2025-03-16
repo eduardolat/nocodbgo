3de48dea7bedce0aff7cbc38e0225df2a3bbb7e0
Luis Eduardo <eduardo.devop@gmail.com>
2025-03-16 00:44:43 +0000

    Reorganize code files

--- constants.go
+++ constants.go
@@ -1,5 +1,21 @@
 package nocodbgo
 
+import "errors"
+
+var (
+	// ErrHTTPClientRequired is returned when attempting to create a client without providing an HTTP client
+	ErrHTTPClientRequired = errors.New("HTTP client is required")
+
+	// ErrBaseURLRequired is returned when attempting to create a client without providing a base URL
+	ErrBaseURLRequired = errors.New("base URL is required")
+
+	// ErrAPITokenRequired is returned when attempting to create a client without providing an API token
+	ErrAPITokenRequired = errors.New("API token is required")
+
+	// ErrRowIDRequired is returned when attempting to perform an operation that requires a row ID without providing one
+	ErrRowIDRequired = errors.New("row ID is required")
+)
+
 // comparisonOperator represents a comparison operator used in query filters
 type comparisonOperator string
 

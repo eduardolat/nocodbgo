1ab9a969d03691935c40affe7f1a2dd39ee2b1fa
Luis Eduardo <eduardo.devop@gmail.com>
2025-03-17 04:18:54 +0000

    Refactor request method to initialize context if nil

--- client.go
+++ client.go
@@ -165,12 +165,10 @@ func (c *Client) request(ctx context.Context, method string, path string, body a
 		reqBody = bytes.NewBuffer(jsonBody)
 	}
 
-	var req *http.Request
-	if ctx != nil {
-		req, err = http.NewRequestWithContext(ctx, method, parsedUrl.String(), reqBody)
-	} else {
-		req, err = http.NewRequest(method, parsedUrl.String(), reqBody)
+	if ctx == nil {
+		ctx = context.Background()
 	}
+	req, err := http.NewRequestWithContext(ctx, method, parsedUrl.String(), reqBody)
 
 	if err != nil {
 		return nil, fmt.Errorf("failed to create request: %w", err)
